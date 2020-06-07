e8e7d4cb41f2ffefa950ca98a86bb6ce8bcd103d
Doug Safford <doug.safford@yahoo.com>
2020-06-07 07:43:00 -0500

    working on ang9 and added 1000images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,25 @@ import { AppComponent } from './app.component';
 import { LoadImgComponent } from './load-img/load-img.component';
 import {HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-    MatButtonModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatDialogModule,
-    MatIconModule,
-    MatInputModule,
-    MatRadioModule
-} from '@angular/material';
+//import {
+//    MatButtonModule,
+//    MatCardModule,
+//    MatCheckboxModule,
+//    MatDialogModule,
+//    MatIconModule,
+//    MatInputModule,
+//    MatRadioModule
+//} from '@angular/material';'
+
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from './app-material.module.ts/app-material.module.ts.module';
+
+import {MatInputModule} from '@angular/material/input'
+import {MatCheckboxModule} from '@angular/material';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
 
 @NgModule({
   declarations: [
